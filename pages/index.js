8a1e2ac505578bc43de1f9d92a1b591192d6eed6
hafizhrf <hafizhsenpai30@gmail.com>
2020-06-25 14:50:19 +0700

    Update index

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
         <h4>Hi There!</h4>
         <div>Vtubers Enthusiast & Degenerate Weebs here!</div>
         <span>I code for money :D</span>
+        <div>Also my github <a href="https://www.github.com/hafizhrf">here</a></div>
       </div>
     </Layout>
   )
