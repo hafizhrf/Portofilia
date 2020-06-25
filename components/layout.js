dd7d24c5c7797ebbfba2bb03f86744c0e9665a4b
hafizhrf <hafizhsenpai30@gmail.com>
2020-06-25 14:41:02 +0700

    Initialization

---
+++ components/layout.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+
+const name = '[Your Name]'
+export const siteTitle = 'H?'
+
+export default function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <meta
+          name="description"
+          content="Do you want to know who i am? please don't.."
+        />
+        <meta name="og:title" content="Hafizh RF" />
+      </Head>
+      <main>{children}</main>
+    </div>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <div>
+        <h4>Hi There!</h4>
+        <div>Vtubers Enthusiast & Degenerate Weebs here!</div>
+        <span>I code for money :D</span>
+      </div>
+    </Layout>
+  )
+}
