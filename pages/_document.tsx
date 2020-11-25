5e2d6b57019b588bed08b1a2e3dae9318a2f3ece
hafizhrf <hafizhsenpai30@gmail.com>
2020-11-26 00:41:15 +0700

    update: meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -45,9 +45,19 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <meta charSet="UTF-8" />
-          <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-          <link rel="shortcut icon" href="/icon.png" />
+          <script src="https://unpkg.com/react/umd/react.production.min.js"></script>
+
+          <script src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"></script>
+
+          <script src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"></script>
+          <meta property="og:title" content="Hafizhrf" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://hafizhrf.vercel.app/" />
+          <meta property="og:image" content="https://avatars3.githubusercontent.com/u/52806525" />
+          <meta property="og:description" content="Do you want to know who i am?" />
+          <meta property="og:site_name" content="Hafizh Rifqi Fabian"></meta>
+          <meta property="og:updated_time" content="1440432930" />
+          <title>Hafizh RF</title>
         </Head>
         <body>
           <Main />
