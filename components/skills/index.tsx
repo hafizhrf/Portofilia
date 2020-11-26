bd861ec03d9da145517ea651a0bec838977b0587
hafizhrf <hafizhsenpai30@gmail.com>
2020-11-26 22:37:30 +0700

    create: skills component

---
+++ components/skills/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import SectionTitle from '../section/sectionTitle'
+// import styled from 'styled-components'
+
+const Skills = (): React.ReactElement => {
+  return (
+    <>
+      <SectionTitle text="skills" />
+      <div>
+        HTML, CSS, JS, PHP, React, NextJs, TS, Laravel, AngularJs
+      </div>
+    </>
+  )
+}
+
+export default Skills
