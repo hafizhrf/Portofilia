040e7dae4685dba4dc48ec7318113ed8a04d54dd
hafizhrf <hafizhsenpai30@gmail.com>
2020-11-26 22:38:30 +0700

    update: main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import Head from 'next/head'
 import Layout from '../components/layout'
+import { TitleText, MutedText } from '../components/text/index.styled'
+import styled from 'styled-components'
+
+import Skills from '../components/skills'
+
+const HiSection = styled.div`
+  margin: 32px 0;
+`
 
 export default function Home(): React.ReactElement {
   return (
     <Layout>
       <Head>
-        <title>H`</title>
+        <title>Hafizh RF</title>
       </Head>
-      <h1>Hi</h1>
-      my name is Hafizh Rifqi Fabian
+      <HiSection>
+        <MutedText>Hi, my name is</MutedText>
+      </HiSection>
+      <TitleText>Hafizh Rifqi Fabian</TitleText>
+      <Skills />
     </Layout>
   )
 }
