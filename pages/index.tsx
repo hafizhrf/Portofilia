c6cc2bfbb22390122c5499883f9517937591d4b7
hafizhrf <hafizhsenpai30@gmail.com>
2020-11-27 22:52:04 +0700

    update: index page with bio and skills stars

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,15 @@ import Layout from '../components/layout'
 import { TitleText, MutedText } from '../components/text/index.styled'
 import styled from 'styled-components'
 
+import Bio from '../components/bio'
 import Skills from '../components/skills'
 
 const HiSection = styled.div`
   margin: 32px 0;
+
+  @media (max-width: 992px) {
+    margin: 0 0 32px;
+  }
 `
 
 export default function Home(): React.ReactElement {
@@ -20,6 +25,7 @@ export default function Home(): React.ReactElement {
         <MutedText>Hi, my name is</MutedText>
       </HiSection>
       <TitleText>Hafizh Rifqi Fabian</TitleText>
+      <Bio text="I am a web programmer from Indonesia who really likes doing new things, I really like playing games or watching anime, still looking for experiences. Nice to meet you!" />
       <Skills />
     </Layout>
   )
